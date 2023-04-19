8f3cf41250fca270f5c678b722f48a77ca753c6a
osias <osias@LAPTOP-NEHDJQO5>
2023-04-19 09:23:57 +0800

    new commit

---
+++ src/main/java/com/mycompany/mmdcprog2/MmdcProg2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mmdcprog2;
+
+/**
+ *
+ * @author osias
+ */
+public class MmdcProg2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("4:02 pm!");
+        System.out.println("4:19 pm!");
+        System.out.println("4:36 pm!");
+        System.out.println("4:48 pm!");
+        System.out.println("4:52 pm!");
+        System.out.println("5:38 pm!");
+     
+    }
+}
---
+++ src/main/java/com/mycompany/mmdcprog2/employee.java
@@ -0,0 +1,14 @@
+package com.mycompany.mmdcprog2;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author osias
+ */
+public class employee {
+    
+}
